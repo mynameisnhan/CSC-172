521d8250470abd04996a2a7916c1d01b316baf17
Nhan Le <nle10147@yahoo.com>
2016-10-07 03:40:25 -0400

    Add files via upload

---
+++ Lab%204%20-%20Singly%20Linked%20List/SLLInterface.java
@@ -0,0 +1,14 @@
+/* Nhan Le
+ * CSC 172
+ * Lab 4: Singly Linked List 
+ */
+
+public interface SinglyLinkedList<T> 
+{
+	public void insert(T x);
+	public void delete(T x);
+	public boolean isPresent(T x); // Changed from "contains" in rubric.
+	public T lookup(T x);
+	public boolean isEmpty();
+	public void printList();
+}
---
+++ Lab%204%20-%20Singly%20Linked%20List/SLLNode.java
@@ -0,0 +1,10 @@
+/* Nhan Le
+ * CSC 172
+ * Lab 4: Singly Linked List 
+ */
+
+public class MyNode<T> 
+{
+	public T data;
+	public MyNode<T> next;
+}
